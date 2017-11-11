732ca1ef9b28d4031195b9a90fa589b0c481b344
liao <patrickliao2007@gmail.com>
2017-11-11 14:21:15 +0800

    Initialization

---
+++ README.md
@@ -0,0 +1,16 @@
+# English vocabulary text-to-speech
+## Requirements
+windows  
+python 2.7  
+gtts  
+## Usage
+1.Install gtts  
+```pip install gtts```  
+note: gtts is blocked by GFW  
+2.Run the script tts.py  
+```python tts.py```  
+Use -h to know the meaning of each arguments  
+```python tts.py -h```  
+3.Copy English vocabulary from Excel  
+4.Press Enter to generate mp3. Input 'e' if you want to exit  
+5.Output files will be in the directory 'output'  
---
+++ tts.py
@@ -0,0 +1,72 @@
+import os
+import argparse
+import win32clipboard as wc
+import win32con
+from gtts import gTTS
+import random
+# from pydub import AudioSegment
+
+
+def get_text():
+    wc.OpenClipboard()
+    d = wc.GetClipboardData(win32con.CF_TEXT)
+    wc.CloseClipboard()
+    return d
+
+
+def format_raw(raw_data, shuffle=False, transpose=True, repeat=1):
+    raw_data = raw_data.strip()
+    lines = raw_data.split('\r\n')
+    for idx, line in enumerate(lines):
+        lines[idx] = line.split('\t')
+    num_each_row = [len(k) for k in lines]
+    column_num = num_each_row[0]
+    a = filter(lambda x: x - column_num, num_each_row)
+    assert len(a) == 0
+    if transpose and not shuffle:
+        lines = [[l[k] for l in lines] for k in range(column_num)]
+    words_queue = reduce(lambda x, y: x + y, lines)
+    if shuffle:
+        random.shuffle(words_queue)
+    if repeat > 1:
+        words_queue = map(lambda x: x + ',' + x, words_queue)
+    words_queue = reduce(lambda x, y: x + ',' + y, words_queue)
+    return words_queue
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', default='list', type=str)
+    parser.add_argument('--slow', default=False, type=bool)
+    parser.add_argument('--shuffle', default=False, type=bool)
+    parser.add_argument('--transpose', default=True, type=bool)
+    parser.add_argument('--begin', default=1, type=int)
+    parser.add_argument('--repeat', default=1, type=int)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_argument()
+    name = args.name
+    shuffle = args.shuffle
+    slow = args.slow
+    transpose = args.transpose
+    begin = args.begin
+    repeat = args.repeat
+    if not os.path.exists('output'):
+        os.mkdir('output')
+    INSTRUCTION = "Copy the text for tts and " \
+                  "press Enter. Input 'e' to exit."
+    while raw_input(INSTRUCTION) != 'e':
+        raw_data = get_text()
+        raw_data = raw_data.lower()
+        words_queue = format_raw(raw_data, transpose=transpose, shuffle=shuffle, repeat=repeat)
+        tts = gTTS(text=words_queue, slow=slow, lang='en')
+        fname = name + '_' + str(begin).zfill(4) + '.mp3'
+        pname = os.path.join('output', fname)
+        tts.save(pname)
+        begin += 1
+
+
+if __name__ == "__main__":
+    main()
